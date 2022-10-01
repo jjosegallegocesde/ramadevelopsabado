d3e0e6cbebd30231a841c91b0d9654b7ec8eaac4
209 <mgilma018@gmail.com>
2022-10-01 11:13:09 -0500

    NTS9-Se agrega la funcion sumar.py  y se imprime el resultado en un if

---
+++ funciones.py
@@ -0,0 +1,16 @@
+# Funcion tradicional para sumar dos numeros
+
+def sumar(numero1, numero2):
+    return numero1 + numero2
+
+
+# Invocando la funcion sumar
+resultado = sumar(5, 10)
+
+
+print( f'La suma es { resultado }' )
+
+if (resultado > 10):
+    print('Ganastes')
+else:
+    print('Perdio')
