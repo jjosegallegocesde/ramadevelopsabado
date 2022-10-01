9bc167a159d9e10d667d9b3e99db3c2e2589e714
209 <209@academicos.cesde.edu.co>
2022-10-01 11:07:23 -0500

    NTS9 se actualizo el archivo funciones

---
+++ funciones.py
@@ -0,0 +1,14 @@
+#funcion tradicional para sumar dos numeros 
+
+def sumar(numero1,numero2):
+    return numero1 + numero2
+
+#invocando la funcion sumar
+
+resultado = sumar(5,10)
+print(f"la suma es: {resultado}")
+
+if resultado > 10:
+    print("ganaste")
+else:
+    print("perdiste")
