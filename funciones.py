30c7718fe67e22d80f403e99771790a4dc7e1a32
dyruiz1 <dyruiz@gmail.com>
2022-10-01 11:11:12 -0500

    se crea funcion

--- funciones.py
+++ funciones.py
@@ -4,4 +4,9 @@ def sumar(numero1, numero2):
 #invocando la funcion sumar
 
 resultado = sumar (10,5)
-print('la suma es: ' resultado)
+print(f"La suma de los números  es {resultado}.")
+
+if resultado > 10:
+    print("Ganaste")
+else:
+    print("Perdiste")
